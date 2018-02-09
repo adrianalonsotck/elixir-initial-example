07302ac9c31d0d06e2f8527f7a84e8405b674de5
Adrián Alonso Vega <adrian.alonsovega@AAVEGA431.local>
2018-02-09 13:42:13 +0100

    Initial Commit

---
+++ lib/budget.ex
@@ -0,0 +1,18 @@
+defmodule Budget do
+  @moduledoc """
+  Documentation for Budget.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Budget.hello
+      :world
+
+  """
+  def current_balance(initial,spending) do
+    initial-spending  
+  end
+end
